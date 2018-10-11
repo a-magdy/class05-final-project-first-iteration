2f2ae20418a426c51c70d06608cda015d86a9207
Majd <majd@Majds-MacBook-Pro.local>
2018-10-11 10:25:40 +0200

    issue 0001 - storybook not working fixed

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,4 +1,12 @@
 import React from "react";
 
-import "./header";
-import "./search";
+import "../App.css";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+import Header from "../components/Header";
+
+// Buttons Story
+storiesOf("Header", module).add("Navigation", () => <Header />);
