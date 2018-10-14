e871a56e646b78c3d8c674ce0bb875f37dce1800
Majd <majd@Majds-MacBook-Pro.local>
2018-10-14 11:05:14 +0200

    Add Map component to storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,6 +7,7 @@ import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 
 import Header from "../components/Header";
+import Map from "../components/Map";
 import InternshipList from "../components/internship/InternshipList";
 
 // Buttons Story
@@ -18,7 +19,7 @@ storiesOf("Buttons and Input", module)
   .add("Buttons", () => <h1>buttons</h1>)
   .add("Search Input", () => <h1>Search</h1>)
   .add("Search Dropdown", () => <h1>Dropdown list</h1>);
-storiesOf("Location", module).add("Map", () => <h1>Map</h1>);
+storiesOf("Location", module).add("Map", () => <Map />);
 storiesOf("Calendar", module)
   .add("Date Input", () => <h1>Date Input</h1>)
   .add("Calendar", () => <h1>Calender</h1>);
